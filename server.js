46198016348aa7992813695adb6aee2d0de2c584
Balint Laszlo <balint.laszlo@t-systems.com>
2020-05-26 22:28:57 +0200

    correction

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/transporter.json'));
+app.use(express.static('./dist/transporter'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: '/dist/transporter.json/' });
+  res.sendFile('index.html', { root: '/dist/transporter/' });
 });
 
 app.listen(process.env.PORT || 8080);
