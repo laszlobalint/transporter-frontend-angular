235ebbe6722727c286630e20af5a5d254f62ba90
Balint Laszlo <balint.laszlo@t-systems.com>
2020-06-03 17:22:26 +0200

    My date range.

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { IAngularMyDpOptions, IMyDateModel } from 'angular-mydatepicker';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public model: IMyDateModel = null;
+  public options: IAngularMyDpOptions = {
+    dateRange: true,
+    dateFormat: 'yyyy.mm.dd.',
+  };
+
+  public onDateChanged(event: IMyDateModel): void {
+    this.model = event;
+  }
+}
